6719bb8d4ad64c1f9c0bf497047236baf5997a79
leavesyang <yangtianxi2011@126.com>
2018-02-04 23:27:32 +0800

    再来一次

--- src/main/java/com/yang/controller/IndexController.java
+++ src/main/java/com/yang/controller/IndexController.java
@@ -20,10 +20,6 @@ import java.util.Map;
 //import org.slf4j.MDC;
 //import org.slf4j.LoggerFactory;
 
-//test github
-//test github
-//test github
-
 @Controller
 @RequestMapping
 public class IndexController {
